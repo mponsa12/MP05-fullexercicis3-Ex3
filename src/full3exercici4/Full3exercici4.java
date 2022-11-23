35a127fc44de8396794d664d8c9c700bf94e5ac2
nil <nil@DESKTOP-C6FGL56>
2022-11-23 01:37:19 +0100

    1r commit. AQUEST COMMIT EN REALITAT SERIA MÉS D'UN COMMIT. Afegit tauler de joc amb la majoria de les funcionalitats

---
+++ src/full3exercici4/Full3exercici4.java
@@ -0,0 +1,12 @@
+package full3exercici4;
+
+public class Full3exercici4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
